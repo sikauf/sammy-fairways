095ca4343f7e6ab1babfdddc8a223b1d73925c61
sikauf <sikauf@umich.edu>
2025-12-29 13:32:37 -0500

    Trying to get it hosted(Change to route.ts)

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,14 +2,16 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
-  const code = searchParams.get("code");
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
 
   if (code) {
     const supabase = await createClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  // send them wherever you want after login
-  return NextResponse.redirect(`${origin}/protected`);
+  const siteUrl =
+    process.env.NEXT_PUBLIC_SITE_URL ?? `${url.protocol}//${url.host}`;
+
+  return NextResponse.redirect(new URL("/protected", siteUrl));
 }
